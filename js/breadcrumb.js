5ba038422520a8b670e018fbe272aedab0d7d6a1
Arendameth <XSlizeMoon@yahoo.com>
2023-02-14 01:56:07 +0200

    Update breadcrumb.js

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -11,7 +11,6 @@ var segments = path.split("/");
 var breadcrumbHTML = "";
 
 breadcrumbHTML += "<a href='https://bislectures.github.io/' style='color: white; text-decoration: underline'>Home</a> / ";
-var isLecturesFound = false;
 
 for (var i = 0; i < segments.length; i++) {
   var segment = segments[i];
@@ -26,21 +25,12 @@ for (var i = 0; i < segments.length; i++) {
     }
     breadcrumbHTML += "<span style='color: white; text-decoration: none'>" + segment + "</span>";
   } else {
-    if (names[segment] === "Lectures") {
-      isLecturesFound = true;
-      breadcrumbHTML += "<a href='https://bislectures.github.io/lectures/" + segments.slice(i + 1).join("/").replace(/ /g, "%20") + "/' style='color: white; text-decoration: underline'>" + names[segment] + "</a>";
+    if (names[segment]) {
+      segment = names[segment];
     } else {
-      if (names[segment]) {
-        segment = names[segment];
-      } else {
-        segment = segment.charAt(0).toUpperCase() + segment.slice(1);
-      }
-      if (isLecturesFound) {
-        breadcrumbHTML += "<a href='https://bislectures.github.io/lectures/" + segments.slice(i).join("/").replace(/ /g, "%20") + "/' style='color: white; text-decoration: underline'>" + segment + "</a>";
-      } else {
-        breadcrumbHTML += "<a href='https://bislectures.github.io/" + segments.slice(1, i + 1).join("/").replace(/ /g, "%20") + "/' style='color: white; text-decoration: underline'>" + segment + "</a>";
-      }
+      segment = segment.charAt(0).toUpperCase() + segment.slice(1);
     }
+    breadcrumbHTML += "<a href='https://bislectures.github.io/" + segments.slice(1, i + 1).join("/").replace(/ /g, "%20") + "/' style='color: white; text-decoration: underline'>" + segment + "</a>";
   }
   if (i !== segments.length - 1) {
     breadcrumbHTML += " / ";
