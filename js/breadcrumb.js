0e0059adf19076915e5b2253e10bf09f5587a1ad
Arendameth <XSlizeMoon@yahoo.com>
2023-02-14 01:20:51 +0200

    Update breadcrumb.js

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -4,14 +4,18 @@ var breadcrumb = document.getElementById("breadcrumb");
 var segments = path.split("/");
 var breadcrumbHTML = "";
 
+breadcrumbHTML += "<a href='https://bislectures.github.io/' style='color: white; text-decoration: underline'>Home</a> / ";
+
 for (var i = 0; i < segments.length; i++) {
   var segment = segments[i];
   if (segment === "") {
     continue;
   }
   if (i === segments.length - 1) {
+    segment = segment.charAt(0).toUpperCase() + segment.slice(1);
     breadcrumbHTML += "<span style='color: white; text-decoration: none'>" + segment + "</span>";
   } else {
+    segment = segment.charAt(0).toUpperCase() + segment.slice(1);
     breadcrumbHTML += "<a href='" + "/".repeat(i) + segment + "/' style='color: white; text-decoration: underline'>" + segment + "</a>";
   }
   if (i !== segments.length - 1) {
